ed1e5f85048bc6514b4b4623a3cc10cf62319b36
Akifumi Fujita <fujita.akifumi@gmail.com>
2020-05-15 01:41:08 +0900

    add RegFile

---
+++ src/main/scala/RegFile.scala
@@ -0,0 +1,33 @@
+package rafi
+
+import chisel3._
+import chisel3.util._
+
+class RegFileReadIF extends Bundle {
+    val rs1 = Input(UInt(5.W))
+    val rs2 = Input(UInt(5.W))
+    val value1 = Output(UInt(64.W))
+    val value2 = Output(UInt(64.W))
+}
+
+class RegFileWriteIF extends Bundle {
+    val write_enable = Input(Bool())
+    val rd = Input(UInt(5.W))
+    val value = Input(UInt(64.W))
+}
+
+class RegFile extends Module {
+    val io = IO(new Bundle {
+        val r = new RegFileReadIF
+        val w = new RegFileWriteIF
+    })
+
+    val m_mem = Mem(32, UInt(64.W))
+
+    io.r.value1 := RegNext(m_mem(io.r.rs1), 0.U)
+    io.r.value2 := RegNext(m_mem(io.r.rs2), 0.U)
+
+    when (io.w.write_enable) {
+        m_mem.write(io.w.rd, io.w.value)
+    }
+}
