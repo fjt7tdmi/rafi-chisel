7d4a28a6166dfff9409599522b135b5cb1025c7e
Akifumi Fujita <fujita.akifumi@gmail.com>
2020-05-15 00:22:56 +0900

    add paths of regsiter number

--- src/main/scala/DecodeStage.scala
+++ src/main/scala/DecodeStage.scala
@@ -6,6 +6,9 @@ import chisel3.util._
 class DecodeStageIF extends Bundle {
     val pc = Output(UInt(64.W))
     val insn = Output(UInt(32.W))
+    val rd = Output(UInt(5.W))
+    val rs1 = Output(UInt(5.W))
+    val rs2 = Output(UInt(5.W))
 }
 
 class DecodeStage extends Module {
@@ -14,6 +17,17 @@ class DecodeStage extends Module {
         val next = new DecodeStageIF
     })
 
+    val rd = Wire(UInt(5.W))
+    val rs1 = Wire(UInt(5.W))
+    val rs2 = Wire(UInt(5.W))
+
+    rd := io.prev.insn(11, 7)
+    rs1 := io.prev.insn(19, 15)
+    rs2 := io.prev.insn(24, 20)
+
     io.next.pc := RegNext(io.prev.pc, 0.U)
     io.next.insn := RegNext(io.prev.insn, 0.U)
+    io.next.rd := RegNext(rd, 0.U)
+    io.next.rs1 := RegNext(rs1, 0.U)
+    io.next.rs2 := RegNext(rs2, 0.U)
 }
--- src/main/scala/RegReadStage.scala
+++ src/main/scala/RegReadStage.scala
@@ -6,6 +6,7 @@ import chisel3.util._
 class RegReadStageIF extends Bundle {
     val pc = Output(UInt(64.W))
     val insn = Output(UInt(32.W))
+    val rd = Output(UInt(5.W))
 }
 
 class RegReadStage extends Module {
@@ -16,4 +17,5 @@ class RegReadStage extends Module {
 
     io.next.pc := RegNext(io.prev.pc, 0.U)
     io.next.insn := RegNext(io.prev.insn, 0.U)
+    io.next.rd := RegNext(io.prev.rd, 0.U)
 }
