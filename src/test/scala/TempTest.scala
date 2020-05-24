a4879fdb83974933dde7de9deaa87d20ab349f0c
Akifumi Fujita <fujita.akifumi@gmail.com>
2020-05-24 16:55:39 +0900

    fix test bench

--- src/test/scala/TempTest.scala
+++ src/test/scala/TempTest.scala
@@ -2,8 +2,9 @@ package rafi
 
 import chisel3._
 import chisel3.iotesters._
+import chisel3.util._
 
-class TempTest extends ChiselFlatSpec {
+class TempTest extends ChiselFlatSpec {    
     behavior of "FetchAddrGenerateStage"
 
     it should "FetchAddrGenerateStage increment PC every cycle" in {
@@ -13,14 +14,26 @@ class TempTest extends ChiselFlatSpec {
             "-tgvo=on", "-tbn=verilator"), () => new Core) {
             core => new PeekPokeTester(core) {
                 reset()
-                expect(core.io.pc, 0.U)
+                
+                var cycle : Int = 0
+                var end : Boolean = false
 
-                for (i <- 0 until 100) {
+                while (!end && cycle < 100) {
                     step(1)
-                    if (core.io.valid == 1.U) {
-                        printf("%08x %08x\n", core.io.pc, core.io.host_io_value)
+                    cycle += 1
+
+                    val valid = peek(core.io.valid)
+                    val pc = peek(core.io.pc).toLong
+                    val host_io_value = peek(core.io.host_io_value).toLong
+
+                    if (valid == 1) {
+                        println(f"$pc%08x $host_io_value%08x")                        
                     }
+
+                    end = valid == 1 && host_io_value != 0
                 }
+
+                //expect(core.io.host_io_value, 1.U)
             }
         } should be (true)
     }
